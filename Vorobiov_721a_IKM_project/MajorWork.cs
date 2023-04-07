f9c87d1fe5d470aaaabde395090a31ff2dc946f5
Vorobiov Dmytro <dimson124@gmail.com>
2023-04-07 11:25:45 +0300

    Finish Task number 4

--- Vorobiov_721a_IKM_project/MajorWork.cs
+++ Vorobiov_721a_IKM_project/MajorWork.cs
@@ -14,6 +14,17 @@ namespace Vorobiov_721a_IKM_project
         private string Data; //вхідні дані
         private string Result; // Поле результату
 
+        private string SaveFileName;// ім’я файлу для запису
+        private string OpenFileName;// ім’я файлу для читання
+        public void WriteSaveFileName(string S)// метод запису даних в об'єкт
+        {
+            this.SaveFileName = S;// запам'ятати ім’я файлу для запису
+        }
+        public void WriteOpenFileName(string S)
+        {
+            this.OpenFileName = S;// запам'ятати ім’я файлу для відкриття
+        }
+
         // Методи
         public void SetTime() // метод запису часу початку роботи програми
         {
