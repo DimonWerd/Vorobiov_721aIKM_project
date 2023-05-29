4c7e421fae0c314a746e8404469996db5aa39ec0
Vorobiov Dmytro <dimson124@gmail.com>
2023-05-29 13:44:39 +0300

    Finish Task number 6

--- Vorobiov_721a_IKM_project/Form1.cs
+++ Vorobiov_721a_IKM_project/Form1.cs
@@ -100,9 +100,10 @@ namespace Vorobiov_721a_IKM_project
 
         private void зберегтиЯкToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (sfdSave.ShowDialog() == DialogResult.OK)// Виклик діалогового вікна збереження файлу 
+            if (sfdSave.ShowDialog() == DialogResult.OK) // Виклик діалогу збереження файлу
             {
-                MajorObject.WriteSaveFileName(sfdSave.FileName); // написання імені файлу
+                MajorObject.WriteSaveFileName(sfdSave.FileName); // Запис імені файлу для збереження
+                MajorObject.Generator();
                 MajorObject.SaveToFile(); // метод збереження в файл
             }
         }
@@ -132,6 +133,29 @@ namespace Vorobiov_721a_IKM_project
                 }
             }
             MessageBox.Show(disk, "Накопичувачі");
+        }
+
+        private void зберегтиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MajorObject.SaveFileNameExists()) // задане ім’я файлу існує?
+                MajorObject.SaveToFile(); // зберегти дані в файл
+            else
+                зберегтиЯкToolStripMenuItem_Click(sender, e);
+        }
+
+        private void новийToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MajorObject.NewRec();
+            tbInput.Clear();// очистити вміст тексту
+            label1.Text = "";
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MajorObject.Modify)
+                if (MessageBox.Show("Дані не були збережені. Продовжити вихід?", "УВАГА",
+                MessageBoxButtons.YesNo) == DialogResult.No)
+                    e.Cancel = true; // припинити закриття
         }
     }
 }
